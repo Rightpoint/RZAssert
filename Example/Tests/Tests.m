09ef0ab5c0d74d667c2749ebf4a092581c8e1ea5
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2014-11-24 16:45:22 -0500

    Prepare repo.

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,38 +6,27 @@
 //  Copyright (c) 2014 mattThousand. All rights reserved.
 //
 
+#import "RZAssert.h"
+
 SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
+static NSString* const nilString = nil;
+static NSString* const emptyString = @"";
+static NSString* const nonEmptyString = @"non-empty string";
 
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
+describe(@"RZASSERT_NIL works", ^{
+    RZASSERT_NIL(nilString);
 
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
-    
-    it(@"will wait and fail", ^AsyncBlock {
-        
-    });
-});
-
-describe(@"these will pass", ^{
-    
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
-    });
-    
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^AsyncBlock {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-            done();
-        });
-    });
+    BOOL worked = NO;
+    @try {
+        RZASSERT_NIL(nonEmptyString);
+    }
+    @catch (NSException *e) {
+        worked = YES;
+    }
+    @finally {
+        expect(worked).to.beTruthy;
+    }
 });
 
 SpecEnd
