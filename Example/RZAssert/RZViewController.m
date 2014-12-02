efb3b4402d4b4c146bd9e5b6ef0d40515ed099aa
mattThousand <matt.d.buckley1212@gmail.com>
2014-12-02 08:55:49 -0500

    Add UIButtons to Main.storyboard to trigger example assertion.

--- Example/RZAssert/RZViewController.m
+++ Example/RZAssert/RZViewController.m
@@ -35,34 +35,84 @@
     }
 }
 
+
+#pragma mark - RZASSERT_TRUE
+
 - (void)assertTrue
 {
     RZASSERT_TRUE(YES);
 }
+
+#pragma mark - RZASSERT_FALSE
 
 - (void)assertFalse
 {
     RZASSERT_FALSE(NO);
 }
 
+#pragma mark - RZASSERT_NIL
+
 - (void)assertNil
 {
     RZASSERT_NIL(nil);
 }
+
+#pragma mark - RZASSERT_NOT_NIL
 
 - (void)assertNotNil:(NSObject *)obj
 {
     RZASSERT_NOT_NIL(obj);
 }
 
+#pragma mark - RZASSERT_NONEMPTY_STRING
+
 - (void)assertNonEmptyString:(NSString *)nonEmptyString
 {
     RZASSERT_NONEMPTY_STRING(nonEmptyString);
 }
 
+#pragma mark - RZASSERT_SUBCLASSES_MUST_OVERRIDE
+
 - (void)assertImplementedBySubclass
 {
     RZASSERT_SUBCLASSES_MUST_OVERRIDE
+}
+
+#pragma mark - Actions
+
+- (IBAction)throwAssertionRZAssertTrue:(id)sender
+{
+    RZASSERT_TRUE(NO);
+}
+
+- (IBAction)throwAssertionRZAssertFalse:(id)sender
+{
+    RZASSERT_FALSE(YES);
+}
+
+- (IBAction)throwAssertionRZAssertNil:(id)sender
+{
+    RZASSERT_NIL(self);
+}
+
+- (IBAction)throwAssertionRZAssertNotNil:(id)sender
+{
+    RZASSERT_NOT_NIL(nil);
+}
+
+- (IBAction)throwAssertionRZAssertNonEmptyString:(id)sender
+{
+    RZASSERT_NONEMPTY_STRING(@"");
+}
+
+- (IBAction)throwAssertionRZAssertSubclassesMustOverride:(id)sender
+{
+    RZASSERT_SUBCLASSES_MUST_OVERRIDE;
+}
+
+- (IBAction)throwAssertionRZAssertShouldNeverGetHere:(id)sender
+{
+    RZASSERT_SHOULD_NEVER_GET_HERE;
 }
 
 @end
