36a7e2a99844ff971a9b1c8e794cee4a1925a65a
Michael Gorbach <michael@mgorbach.name>
2014-12-02 11:30:09 -0500

    Rename and set org.

--- Example/RZAssert/RZAppDelegate.m
+++ Example/RZAssert/RZAppDelegate.m
@@ -2,7 +2,7 @@
 //  RZAppDelegate.m
 //  RZAssert
 //
-//  Created by CocoaPods on 11/24/2014.
+//  Created by Raizlabs on 11/24/2014.
 //  Copyright (c) 2014 mattThousand. All rights reserved.
 //
 
