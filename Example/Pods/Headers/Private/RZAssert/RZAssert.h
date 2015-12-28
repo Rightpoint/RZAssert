344ffa1976f900b4f098553ec1e2969a22082da6
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2015-12-28 18:13:08 -0500

    Modernize project. Update Example project pods using latest CocoaPods.

---
+++ Example/Pods/Headers/Private/RZAssert/RZAssert.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/RZAssert.h
--- Example/Pods/Target Support Files/Pods-RZAssert-RZAssert/Pods-RZAssert-RZAssert-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RZAssert_RZAssert : NSObject
-@end
-@implementation PodsDummy_Pods_RZAssert_RZAssert
-@end
